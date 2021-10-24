724edb490b80a6e31ee696bdecc18b82a6404d24
Redevice <redevice7@gmail.com>
2021-10-24 23:40:00 +0600

    old code

---
+++ Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace sha1hasher
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tbMac = new System.Windows.Forms.TextBox();
+            this.bResult = new System.Windows.Forms.Button();
+            this.tbResult = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tbMac
+            // 
+            this.tbMac.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tbMac.Font = new System.Drawing.Font("Arial Narrow", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbMac.Location = new System.Drawing.Point(0, 0);
+            this.tbMac.Margin = new System.Windows.Forms.Padding(2);
+            this.tbMac.Name = "tbMac";
+            this.tbMac.Size = new System.Drawing.Size(509, 29);
+            this.tbMac.TabIndex = 0;
+            // 
+            // bResult
+            // 
+            this.bResult.Dock = System.Windows.Forms.DockStyle.Top;
+            this.bResult.Font = new System.Drawing.Font("Arial", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.bResult.Location = new System.Drawing.Point(0, 0);
+            this.bResult.Margin = new System.Windows.Forms.Padding(2);
+            this.bResult.Name = "bResult";
+            this.bResult.Size = new System.Drawing.Size(509, 43);
+            this.bResult.TabIndex = 1;
+            this.bResult.Text = "Generate SHA1SUM";
+            this.bResult.UseVisualStyleBackColor = true;
+            this.bResult.Click += new System.EventHandler(this.bResult_Click);
+            // 
+            // tbResult
+            // 
+            this.tbResult.Dock = System.Windows.Forms.DockStyle.Top;
+            this.tbResult.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.tbResult.Location = new System.Drawing.Point(0, 43);
+            this.tbResult.Margin = new System.Windows.Forms.Padding(2);
+            this.tbResult.Multiline = true;
+            this.tbResult.Name = "tbResult";
+            this.tbResult.ReadOnly = true;
+            this.tbResult.Size = new System.Drawing.Size(509, 67);
+            this.tbResult.TabIndex = 2;
+            this.tbResult.Click += new System.EventHandler(this.tbResult_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.tbResult);
+            this.panel1.Controls.Add(this.bResult);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 87);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(509, 111);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel2.Location = new System.Drawing.Point(0, 0);
+            this.panel2.Margin = new System.Windows.Forms.Padding(2);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(509, 59);
+            this.panel2.TabIndex = 4;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.tbMac);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 59);
+            this.panel3.Margin = new System.Windows.Forms.Padding(2);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(509, 28);
+            this.panel3.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(5F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 198);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Font = new System.Drawing.Font("Arial Narrow", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SHA1HASHER";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbMac;
+        private System.Windows.Forms.Button bResult;
+        private System.Windows.Forms.TextBox tbResult;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sha1hasher
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static string Hash(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length);
+
+                foreach (byte b in hash)
+                {
+                    var hex = b.ToString("x2");
+                    sb.Append(hex);
+                }
+
+                return sb.ToString();
+            }
+        }
+
+        private void bResult_Click(object sender, EventArgs e)
+        {
+            string inputText = tbMac.Text;
+            inputText = inputText + "\n";
+            string HashSum = Hash(inputText);
+            tbResult.Text = HashSum;
+        }
+
+        private void tbResult_Click(object sender, EventArgs e)
+        {
+            tbResult.SelectAll();
+        }
+    }
+}
